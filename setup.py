90995d8d46d25c0ef0a069a660dd3ed4e40ad1e7
Federico Galatolo <galatolo.federico@gmail.com>
2019-09-10 16:14:37 +0200

    typo fix

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     install_requires=[
         "numpy",
-        "pyqtgraph"
+        "pyqtgraph",
         "Pillow"
     ],
     classifiers=[
