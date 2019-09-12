6db6fe71d1cfd6f20792a290c4932c79cc002e05
Giuliano Peraz <giuliano.peraz@gmail.com>
2019-09-12 16:58:32 +0200

    Added skeleton for testing stigmergy and some change to test_MASS
    I added initialization code and test code for unittest to run properly.
    test_stigmergy.py is the base for unit testing of the next class
    stigmergy that has to be implemented yet.

--- tests/test_MASS.py
+++ tests/test_MASS.py
@@ -1,7 +1,22 @@
-import unittest
+import unittest as ut
+import numpy as np
+from mass import MASS
 
-Class TestMASS(unittest.TestCase):
-    def test_reward_collision_with_target:
-        pass
+class TestMASS(ut.TestCase):
+    def test_reward_collision_with_target(self):
+        test_obj = MASS(bitmap = './tests/test-with-2-levels.bmp',
+                batch_size = 1,
+                observation_range = 5,
+                agent_size = 0,
+                amount_of_agents = 3,
+                stigmergy_evaporation_speed = np.array([10, 20, 30]),
+                stigmergy_colours = np.array([[255,64,0],[255,128,0],[255,255,0]]),
+                inertia = 0,
+                collision_detection = np.array([True, True]),
+                max_steps = 1000,
+                rendering_allowed = True
+                )
+        test_obj.__targets = 0
+        self.assertTrue(test_obj.__targets)
     
-    
+        
---
+++ tests/test_stigmergy.py
@@ -0,0 +1,6 @@
+import unittest as ut
+import numpy as np
+
+class TestStigmergy(ut.TestCase):
+    def test_that_do_not_pass(self):
+        self.assertTrue(True)
